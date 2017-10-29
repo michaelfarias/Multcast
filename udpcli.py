5e608f1ceb74bc46c027c233e64dc4c9ea1a5103
michaelfarias <michaelfarias01@gmail.com>
2017-10-29 17:52:14 -0200

    Add files via upload

---
+++ udpcli.py
@@ -0,0 +1,13 @@
+import sys, socket
+ 
+if __name__ == '__main__':
+    try:
+        addr = sys.argv[1]
+        port = int(sys.argv[2])
+        buff = sys.argv[3]
+    except IndexError:
+        print 'use: %s addr port buff' % sys.argv[0]
+        sys.exit(1)
+ 
+    fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    fd.sendto(buff, (addr, port))
